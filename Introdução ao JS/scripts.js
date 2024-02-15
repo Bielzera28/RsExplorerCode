fb2a8785508b0f4ef670c4a6d96b6c1f75d0c761
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-15 16:00:33 -0300

    Retornando valores dentro da função

--- Introdução ao JS/scripts.js
+++ Introdução ao JS/scripts.js
@@ -12,8 +12,21 @@ function createPhrases() {
 createPhrases()
 
 //Parâmetros e Argumentos
-const sum = function(number1, number2) { //definindo parâmetros dentro do ()
+/*const sum = function(number1, number2) { //definindo parâmetros dentro do ()
   console.log(number1 + number2)
 }
 
-sum(2,3) // argumentos
+sum(2,3) // argumentos*/
+
+//Retornando valores dentro da função
+const sum = function(number1, number2) {
+  total = number1 + number2
+  return total
+}
+
+let number1 = 35
+let number2 = 25
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
