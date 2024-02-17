4a6cd97e4a60dd7dc21c585dcc5d13db67ab7a50
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-17 13:12:35 -0300

    String em number e number em string

--- Introdução ao JS/scripts.js
+++ Introdução ao JS/scripts.js
@@ -3,3 +3,9 @@
 //Type conversion coersion
 console.log("9" + 5) //converte 5 em string pra não ter problema = 95
 console.log(Number("9") + 5)
+
+//String em números
+let string = "123"
+console.log(Number(string))
+let number = 321
+console.log(String(number))
