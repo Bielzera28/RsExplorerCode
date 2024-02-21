2c1b12fc9b659c826e06b6c09eef9b15f26df853
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-20 22:47:08 -0300

    Switch

--- Introdução ao JS/scripts.js
+++ Introdução ao JS/scripts.js
@@ -13,3 +13,28 @@ if(highTemperature) {
   console.log("Saudável")
 }
 
+//Switch
+function calculate(number1, operator, number2) {
+  let result
+
+  switch(operator) {
+    case "+":
+      result = number1 + number2
+      break
+    case "-":
+      result = number1 - number2
+      break
+    case "*":
+      result = number1 * number2
+      break
+    case "/":
+      result = number1 / number2
+      break
+    default:
+      console.log("Não implementado")
+      break
+  }
+  return result
+}
+
+console.log(calculate(4, "*", 8))
