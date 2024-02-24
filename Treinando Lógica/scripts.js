58c8399687e57bff46d6dbf831b5b9c171ba7044
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-24 13:23:18 -0300

    Somar números + concatenação + group operator

--- Treinando Lógica/scripts.js
+++ Treinando Lógica/scripts.js
@@ -1,3 +1,6 @@
-// Perguntando nome do usuário e escreva a mensagem: "Olá ..."
-let nome = prompt("Qual o seu nome?")
-alert("Olá, " + nome)
+//Somar dois números
+
+alert("Iremos somar 2 números")
+let numberOne = prompt("Digite o número 1:");
+let numberTwo = prompt("Digite o número 2:");
+alert("A soma dos números é: " + (Number(numberOne) + Number(numberTwo)));
