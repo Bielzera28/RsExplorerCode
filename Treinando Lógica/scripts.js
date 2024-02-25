4940a158c32a7ddf7e5d651a0a256b413a8dbdfa
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-25 19:28:29 -0300

    Operações matemáticas

--- Treinando Lógica/scripts.js
+++ Treinando Lógica/scripts.js
@@ -1,6 +1,9 @@
-//Somar dois números
-
-alert("Iremos somar 2 números")
-let numberOne = prompt("Digite o número 1:");
-let numberTwo = prompt("Digite o número 2:");
-alert("A soma dos números é: " + (Number(numberOne) + Number(numberTwo)));
+//Operações matemáticas
+alert("Operações matemáticas:")
+let number1 = prompt("Qual será o primeiro número ?")
+let number2 = prompt("Qual será o segundo número?")
+alert("A soma dos dois números é " + (Number(number1) + Number(number2)))
+alert("A subtração dos dois números é " + (Number(number1) - Number(number2)))
+alert("A divisão dos dois números é " + (Number(number1) / Number(number2)))
+alert("A multiplicação dos dois números é " + (Number(number1) * Number(number2)))
+alert("O resto da divisão dos dois números é " + (Number(number1) % Number(number2)))
---
+++ Treinando Lógica/somar.js
@@ -0,0 +1,6 @@
+//Somar dois números
+
+alert("Iremos somar 2 números")
+let numberOne = prompt("Digite o número 1:");
+let numberTwo = prompt("Digite o número 2:");
+alert("A soma dos números é: " + (Number(numberOne) + Number(numberTwo)));
