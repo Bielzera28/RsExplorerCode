3f56e1e8674a9cbb52f48b504968d7d462fc1826
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-26 21:13:03 -0300

    Lista de compras

--- Treinando Lógica/script.js
+++ Treinando Lógica/script.js
@@ -1,18 +1,10 @@
-//Calculando media de um estudante
-alert("Vamos calcular sua média! ")
-let student = prompt("Qual o seu nome?")
-let n1 = prompt("Qual a sua nota na primeira prova ?")
-let n2 = prompt("Qual a sua nota na segunda prova ?")
-let n3 = prompt("Qual a sua nota na terceira prova ?")
+//Criando lista de compras
+let items = [];
 
-let average = (Number(n1) + Number(n2) + Number(n3)) / 3
-let result = average >= 6
-average = average.toFixed(1)
+for(let item = 0; item < 10; item++) {
+  let itemName = prompt("Digite o item " + (item + 1))
+  items[item] = itemName
 
-if (result) {
-  alert("Parabéns " + student + "! Sua média foi " + average)
-} else if (average < 3) {
-  alert(student + ", você está reprovado!")
-} else {
-  alert(student + ", estude para sua prova de recuperação! Sua média foi " + average) 
 }
+
+alert(items)
