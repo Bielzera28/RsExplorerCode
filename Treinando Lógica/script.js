cd6441ef1f5082635eb074f0819f315bdadb3c41
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-02-26 21:33:35 -0300

    Jogo da adivinhação

--- Treinando Lógica/script.js
+++ Treinando Lógica/script.js
@@ -1,10 +1,18 @@
-//Criando lista de compras
-let items = [];
+//Jogo da adivinhação
+let result = prompt("Adivinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+const match = Number(result) == randomNumber
+let attempts = 1;
 
-for(let item = 0; item < 10; item++) {
-  let itemName = prompt("Digite o item " + (item + 1))
-  items[item] = itemName
-
+while(Number(result) != randomNumber) {
+  result = prompt("Erro, tente novamente")
+  attempts++
 }
 
-alert(items)
+if (attempts != 1) {
+  alert(`Parabens você acertou em ${attempts} tentativas`)
+  
+} else {
+  alert(`Parabens você acertou em 1 tentativa`)
+}
+
