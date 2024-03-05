8b465127ee072eabbb9184e169e76281eb0b84be
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-03-04 21:41:15 -0300

    Intensivão  - JS (pt 1)

---
+++ Intensivão/JS/script.js
@@ -0,0 +1,46 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+console.log("Hello World!")
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let number1 = 10
+let number2 = 20
+
+console.log(number1 + number2)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+let value = {
+  name: "Gabriel",
+  age: 25
+}
+
+if (typeof value.age === 'number') {
+  console.log("É um número")
+} else {
+  console.log("Não é um número")
+}
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let student = {
+  name: "Gabriel",
+  age: 25
+}
+
+if (typeof value.age === 'string') {
+  console.log("É uma string")
+} else {
+  console.log("Não é uma string")
+}
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+let booleano = true
+
+if (typeof booleano === 'boolean') {
+  console.log("É um booleano")
+} else {
+  console.log("Não é um booleano")
+}
