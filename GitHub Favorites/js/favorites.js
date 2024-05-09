1f8934c829df570ee480b7fd3e7104b5b8d940f0
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-05-09 19:07:22 -0300

    Ajustes finais

--- GitHub Favorites/js/favorites.js
+++ GitHub Favorites/js/favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-    .then(data => data.json())
-    .then(({ login, name, public_repos, followers }) => ({
-      login,
-      name,
-      public_repos,
-      followers
-    }))
-  }
-}
+import { GithubUser } from "./githubUser.js";
 
 // classe com logica dos dados e como serão estruturados
 export class Favorites {
@@ -30,6 +17,12 @@ export class Favorites {
 
   async add(username) {
     try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+      if(userExists) {
+        throw new Error("Usuário já cadastrado!")
+      }
+
       const user = await GithubUser.search(username)
       if(user.login === undefined) {
         throw new Error("Usuário não encontrado!")
@@ -48,8 +41,7 @@ export class Favorites {
 
 
   delete(user) {
-    const filteredEntries = this.entries.filter((entry) =>{
-      entry.login !== user.login})
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
     this.entries = filteredEntries
     this.update()
     this.save()
@@ -83,6 +75,7 @@ export class FavoritesView extends Favorites {
       row.querySelector(".user img").src = `https://github.com/${user.login}.png`
       row.querySelector(".user img").alt = `Imagem de ${user.name}`
       row.querySelector(".user p").textContent = user.name
+      row.querySelector(".user a").href = `https://github.com/${user.login}`
       row.querySelector(".user span").textContent = user.login
       row.querySelector(".repositories").textContent = user.public_repos
       row.querySelector(".followers").textContent = user.followers
